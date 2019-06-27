c96797fa8d7f20e9ad398a232c91f875f43d2833
T <test@mail.com>
2019-06-27 15:46:14 -0700

    made some change

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,19 @@ import datetime
 class BaseModel:
     """Base class"""
 
-    def __init__(self, id=None, created_at=None, updated_at=None, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Initializes attributes for class BaseModel
         Args:
             id: identification number
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if not kwargs and kwargs != {}:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ """
@@ -31,10 +35,10 @@ class BaseModel:
     def to_dict(self):
         """ """
 
-        d = self.__dict__.copy
+        d = self.__dict__.copy()
         d['__class__'] = self.__class__.__name__
         d['created_at'] = self.created_at.isoformat()
         d['updated_at'] = self.updated_at.isoformat()
         return d
 
-    def __init__(self, *args, **kwargs):
+    
