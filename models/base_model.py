6042732bcb0939bd8f98b2bae32b8989e6eeb490
T <test@mail.com>
2019-06-27 17:17:41 -0700

    changed variable name

--- models/base_model.py
+++ models/base_model.py
@@ -29,8 +29,8 @@ class BaseModel:
     def __str__(self):
         """ """
 
-        cls = self.__class__.__name__
-        return ("[{}] ({}) {}".format(cls, self.id, self.__dict__))
+        class_name = self.__class__.__name__
+        return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
     def save(self):
         """ """
