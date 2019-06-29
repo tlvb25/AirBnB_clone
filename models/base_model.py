804cbfa7e72e7e2b788145509b395a4e9f302c52
Marc Cavigli <mcavigli@gmail.com>
2019-06-29 00:29:03 +0000

    Updated for task 5

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Module that holds class BaseModel"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -25,6 +26,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ """
@@ -36,6 +38,7 @@ class BaseModel:
         """ """
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ """
