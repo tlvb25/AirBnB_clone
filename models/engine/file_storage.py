54a50a30d8088ba39c89bfd6c01997bbd5d1b155
Marc Cavigli <mcavigli@gmail.com>
2019-07-02 17:41:12 +0000

    Added class names to dictionary and imported all classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,11 @@
 
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 import json
 from os import path
 
@@ -32,7 +37,12 @@ class FileStorage:
         dict_of_dicts = {}
         classes = {
             "BaseModel": BaseModel,
-            "User": User}
+            "User": User,
+            "Amenity": Amenity,
+            "City": City,
+            "Place": Place,
+            "Review": Review,
+            "State": State}
         try:
             with open(self.__file_path, "r") as r:
                 dict_of_dicts = json.load(r)
