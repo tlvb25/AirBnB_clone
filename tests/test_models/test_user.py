a5872135c8f6d9885df60a88d7ce9413c1fb0f4b
T <test@mail.com>
2019-07-03 02:16:52 -0700

    code update

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Unittest for BaseModel User """
 import unittest
-from models.user import User
+from datetime import datetime
+import models
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class TestUser(unittest.TestCase):
     """ Runs Test for User.py """
@@ -16,6 +18,42 @@ class TestUser(unittest.TestCase):
         self.a.first_name = "Betty"
         self.a.last_name = "Holberton"
 
+        self.attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
+
     def tearDown(self):
         print('tear down')
 
@@ -26,4 +64,8 @@ class TestUser(unittest.TestCase):
         self.assertIsInstance(self.a, BaseModel)
 
     def Test_attribute(self):
-        self.
+        mark = User()
+        for k, v in self.attributes.items():
+            self.assertIn(k, self.attributes)
+            self.assertEqual(type(getattr(mark, k, None)), v)
+    
